4e23fbfd86487fd4e33ea12ed23dba92e34d9387
VyDink <vy.dinh.2929@gmail.com>
2025-04-09 16:06:42 +0700

    Ex-5 Pass all tests w/ leapYear condition commended

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+// 3 điều kiện để thỏa mãn là một năm nhuận ( leapYear )
 
+// 1. Năm đó chia hết cho 4 và không chia hết cho 100.
+// 2. Năm đó chia hết cho 400 thì chắc chắn là năm nhuận.
+// 3. Nếu chỉ chia hết cho 100 mà ko chia đc cho 400 thì ko p năm nhuận.
+
+const leapYears = function (x) {
+  if ((x % 4 === 0 && x % 100 !== 0) || x % 400 === 0) {
+    const leapYearsCheck = true;
+    return leapYearsCheck;
+  } else return false;
 };
 
 // Do not edit below this line
