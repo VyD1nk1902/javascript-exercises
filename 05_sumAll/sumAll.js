79bb9ebf2455848660ca73057a25ef9ae052c745
VyDink <vy.dinh.2929@gmail.com>
2025-04-09 15:34:31 +0700

    Ex5 - Pass all tests 1. Tạo một biến chứa kết quả. 2. Tạo một vòng lặp for (let i = x, i <= n, i++). 3. trong vòng lặp sẽ thực thi result += i. 4. Trả về kết quả result.
    5. Tạo điều kiện nếu không p số nguyên thì ERROR.
    6. Tạo điều kiện nếu không phải số dương thì ERROR.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (x, n) {
+  if (!Number.isInteger(x) || !Number.isInteger(n)) return "ERROR";
+  if (x < 0 || n < 0) return "ERROR";
+  let result = 0;
+  if (x < n) {
+    for (let i = x; i <= n; i++) {
+      result += i;
+    }
+    return result;
+  } else if (x > n) {
+    for (let i = n; i <= x; i++) {
+      result += i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
