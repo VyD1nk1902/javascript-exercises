f31eb00ef61f41fe7a33830aa5155825b9df78c4
VyDink <vy.dinh.2929@gmail.com>
2025-04-09 16:27:06 +0700

    Ex-7 Pass all tests
    Công thức qui đổi lấy trên wiki.
    Dùng Math.round(temp * 10)/10 -> làm tròn đến 1 chữ
    số thập phân.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  const celsiusConvert = Math.round((x - 32) * (5 / 9) * 10) / 10;
+  return celsiusConvert;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (y) {
+  const fahrenheitConvert = Math.round((y * (9 / 5) + 32) * 10) / 10;
+  return fahrenheitConvert;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
