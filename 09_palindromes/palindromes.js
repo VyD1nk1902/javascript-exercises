a0c6c650a12d95623a32c4cf0222074eecc86487
VyDink <vy.dinh.2929@gmail.com>
2025-04-18 16:06:50 +0700

    Ex-09 passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  //   const string = word.replace(/[ ,!.]/g, ""); // global scope thay thế tất cả cá dâu bên trong /[]/ thành , ""
+  //   const newString = string.toLowerCase();
+  //   const splitString = newString.split("");
+  //   const reverseString = splitString.reverse().join("");
+  const wordFilter = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanWord = word
+    .toLowerCase()
+    .split("")
+    .filter((character) => wordFilter.includes(character))
+    .join("");
+  const reversedString = cleanWord.split("").reverse().join("");
+  return cleanWord === reversedString;
 };
 
 // Do not edit below this line
