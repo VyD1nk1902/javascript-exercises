4ad7edc82c28294dbeff41a5e1a06f748ab9214d
VyDink <vy.dinh.2929@gmail.com>
2025-04-18 14:33:33 +0700

    Done 08 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((a, b) => {
+    return a + b;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((a, b) => {
+    return a * b;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 1 || a === 0) {
+    return 1;
+  }
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
